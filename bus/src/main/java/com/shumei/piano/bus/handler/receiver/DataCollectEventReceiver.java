c6334deea8e6b586b1ba974d063c5fad14ea9378
Eden Zhang <boyzhangyi@aliyun.com>
2018-12-21 20:31:39 +0800

    添加服务注册中心

---
+++ bus/src/main/java/com/shumei/piano/bus/handler/receiver/DataCollectEventReceiver.java
@@ -0,0 +1,32 @@
+package com.shumei.piano.bus.handler.receiver;
+
+import com.shumei.piano.bus.event.DataCollectEvent;
+import com.shumei.piano.bus.handler.EventHandler;
+import com.shumei.piano.bus.handler.EventSink;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+/**
+ * 数据采集事件接收器
+ *
+ * @author Zhang Yi on 2018/12/20
+ * @version Piano V1.0.0
+ */
+@EnableBinding(EventSink.class)
+public class DataCollectEventReceiver
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataCollectEventReceiver.class);
+
+    /**
+     * 处理数据采集事件
+     *
+     * @param event 数据采集事件
+     * @author Zhang Yi on 2018/12/20
+     */
+    @EventHandler(DataCollectEvent.class)
+    public void handleEvent(DataCollectEvent event)
+    {
+        LOGGER.info("Receive a bus event: {}", event);
+    }
+}
---
+++ eureka-server/src/main/java/com/shumei/piano/bus/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 服务注册中心包
+ *
+ * @author Zhang Yi on 2018/12/21
+ * @version Piano V1.0.0
+ */
+package com.shumei.piano.bus;
