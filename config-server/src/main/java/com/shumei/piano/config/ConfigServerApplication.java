d1172ee3fac0df7546c00135a37e30de8b89a740
Eden Zhang <boyzhangyi@aliyun.com>
2019-01-05 20:31:06 +0800

    添加配置中心

--- config-server/src/main/java/com/shumei/piano/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/shumei/piano/config/ConfigServerApplication.java
@@ -1,27 +1,27 @@
-package com.shumei.piano.eureka;
+package com.shumei.piano.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
- * 服务注册中心启动类
+ * 配置中心启动入口
  *
- * @author Zhang Yi on 2018/12/21
+ * @author Zhang Yi on 2018/12/22
  * @version Piano 1.0.0
  */
-@EnableEurekaServer
+@EnableConfigServer
 @SpringBootApplication
-public class EurekaServerApplication
+public class ConfigServerApplication
 {
     /**
-     * 服务注册中心启动入口
+     * 配置中心启动入口
      *
      * @param args 服务注册中心启动参数
      * @author Zhang Yi on 2018/12/21
      */
     public static void main(String[] args)
     {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
