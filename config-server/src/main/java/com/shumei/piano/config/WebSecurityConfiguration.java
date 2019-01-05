829db6bfcfa6ffc7a4de6191cd3dfdc3e22f9a3e
Eden Zhang <boyzhangyi@aliyun.com>
2019-01-05 21:23:08 +0800

    配置中心

---
+++ config-server/src/main/java/com/shumei/piano/config/WebSecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.shumei.piano.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Spring Security默认开启了所有CSRF攻击防御，需要禁用/eureka的防御
+ *
+ * @author Zhang Yi on 2019/1/5
+ * @version Piano 1.0.0
+ */
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
+{
+    /**
+     * Web安全配置加载
+     *
+     * @param httpSecurity Http安全策略
+     * @throws Exception 异常信息
+     * @author Zhang Yi on 2018/12/23
+     */
+    @Override
+    protected void configure(HttpSecurity httpSecurity)
+            throws Exception
+    {
+        httpSecurity.csrf().ignoringAntMatchers("/**")
+                    .and().authorizeRequests().anyRequest().authenticated()
+                    .and().httpBasic();
+        super.configure(httpSecurity);
+    }
+}
